5eb1957ebd7a9716e650231697c1c995c2e09076
aluno <aluno@aluno-desktop>
2013-03-19 08:25:51 -0300

    Teste ....

---
+++ ltp3/src/br/edu/ifnmg/tads/ltp3/Ltp3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.ltp3;
+
+/**
+ *
+ * @author aluno
+ */
+public class Ltp3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
